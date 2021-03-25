43985b76e0c4396550f030e3a7b72c41775dc319
Muhamad Fahrur Rasyid <mfahrurashyid@gmail.com>
2021-03-24 22:20:15 -0500

    Repair

--- backend/scripts/entrypoint-prod.sh
+++ backend/scripts/entrypoint-prod.sh
@@ -4,3 +4,6 @@ python manage.py migrate --no-input
 python manage.py runscript load
 
 python backend/manage.py runserver 0.0.0.0:$PORT
+python backend/manage.py makemigrations --no-input
+python backend/manage.py migrate --no-input
+python backend/manage.py runscript load
